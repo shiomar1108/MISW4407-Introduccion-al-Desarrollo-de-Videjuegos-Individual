05d20f510833bcc1297d7951abf0b1a0692fd010
Shiomar Salazar <33112192+shiomar1108@users.noreply.github.com>
2024-04-25 13:09:23 -0600

    Se agregan archivos de otro repositorio

---
+++ Practica_Semana3/src/ecs/systems/s_player_state.py
@@ -0,0 +1,32 @@
+import esper
+from src.ecs.components.c_animation import CAnimation
+from src.ecs.components.c_player_state import CPlayerState, PlayerState
+from src.ecs.components.c_velocity import CVelocity
+
+def sustem_player_state(world:esper.World):
+    components = world.get_components(CVelocity, CAnimation, CPlayerState)
+
+    for _, (c_v,c_a, c_p_s) in components:
+        if c_p_s.state == PlayerState.IDLE:
+            _do_idle_state(c_v,c_a,c_p_s)
+        elif c_p_s.state == PlayerState.MOVE:
+            _do_move_state(c_v,c_a,c_p_s)
+
+
+def _do_idle_state(c_v:CVelocity, c_a:CAnimation, c_p_s:CPlayerState):
+    _set_animation(c_a, 1)
+    if c_v.vel.magnitude_squared() > 0:
+        c_p_s.state = PlayerState.MOVE
+
+
+def _do_move_state(c_v:CVelocity, c_a:CAnimation, c_p_s:CPlayerState):
+    _set_animation(c_a, 0)
+    if c_v.vel.magnitude_squared() <= 0:
+        c_p_s.state = PlayerState.IDLE
+
+def _set_animation(c_a:CAnimation, num_anim:int):
+    if c_a.current_animation == num_anim:
+        return
+    c_a.current_animation = num_anim
+    c_a.current_animation_time = 0
+    c_a.curr_frame = c_a.animations_list[c_a.current_animation].start
